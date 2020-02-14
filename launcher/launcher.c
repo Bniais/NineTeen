5b3d85e579885972d0767e74cb31efbf472c8ec6
samlastar <samymahi@me.com>
2020-02-14 11:01:26 +0100

    entrer pour ce connecter

--- launcher/launcher.c
+++ launcher/launcher.c
@@ -212,6 +212,15 @@ void connexion(SDL_Renderer *renderer, char **token)
 		{
 			exit(0);
 		}
+		else if ( etatIdentifant == TF_RETURN || etatMotDePasse  == TF_RETURN )
+		{
+			if ( !connectWithUsername(token,identifiant,motDePasse) )
+			{
+				pressConnexion = SDL_TRUE;
+				etatMotDePasse = RESPONDER_FALSE;
+				etatIdentifant = RESPONDER_FALSE;
+			}
+		}
 		else if(etatIdentifant == TF_MOUSE_OUT_CLICK || etatMotDePasse  == TF_MOUSE_OUT_CLICK)
 		{
 			if(mouse.x && mouse.y)
@@ -229,10 +238,6 @@ void connexion(SDL_Renderer *renderer, char **token)
 				}
 				else if ( TF_ClickIn( targetConnect , mouse) )
 				{
-					time_t t = time(NULL);
-					struct tm tm = *localtime(&t);
-					printf("IL EST : %d-%02d-%02d %02d:%02d:%d\n",tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour , tm.tm_min, tm.tm_sec );
-
 					if ( !connectWithUsername(token,identifiant,motDePasse) )
 					{
 						pressConnexion = SDL_TRUE;
