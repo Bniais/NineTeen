05d0d18c6d32d7f3addb77b10e656d584337d1dd
Bniais <hugo.lecomte72@hotmail.fr>
2020-03-27 23:10:20 +0100

    linux room

--- room/opengl/import.h
+++ room/opengl/import.h
@@ -1,9 +1,21 @@
 #include <assimp/cimport.h>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
+#ifdef __APPLE__
+	#define GL_SILENCE_DEPRECATION
+	#include <OpenGL/gl.h>
+	#include <OpenGL/OpenGL.h>
+	#include <GLUT/glut.h>
+#endif
+#ifdef __linux
+	#include <GL/gl.h>
+	#include <GL/glu.h>
+	#include <GL/glut.h>
+  #define M_PI 3.1415
+#endif
 
-#define GL_SILENCE_DEPRECATION
-#include <OpenGL/gl.h>
+
+//#include <OpenGL/gl.h>
 
 int aiImportModel (const char* path,const C_STRUCT aiScene **scene);
 
