98d2c0962ecdd2dcff5e3df657e66e1d6b0eea4f
Samy Mahi <samymahi@imac-de-samy.home>
2020-04-02 18:55:10 +0200

    tetris lib SDL2/SDL...

--- games/5_tetris/tetris.c
+++ games/5_tetris/tetris.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_mixer.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_image.h>
 
 #include <stdlib.h>
 #include <stdio.h>
