8ec5f305a5026080107b409873dfdd4ee01bc978
Ikenaa <rayyantounsy@gmail.com>
2020-04-03 10:33:22 +0200

    Equilibrage et bonus point

--- games/7_asteroid/config.h
+++ games/7_asteroid/config.h
@@ -16,18 +16,18 @@ const Vector2f UNDEFINED = {-500, -500};
 #define LEFT 1
 #define RIGHT 2
 #define RAYON_VAISS 10
-#define DECELERATION 1.017
-#define TURN_AMMOUNT 0.06
+#define DECELERATION 1.015
+#define TURN_AMMOUNT 0.08
 #define RECHARGE_TIR (FRAMES_PER_SECOND/2)
 #define VITESSE 10
-#define ACCEL 0.2
+#define ACCEL 0.35
 #define BASE_ANGLE 3 * PI / 2
 
 //asteroid
 #define NB_TAILLE 3
 #define DIST_2ASTEROID 30
-#define DIST_VAISSEAU_ASTEROID 200
-typedef enum{PETIT, MOYEN, GRAND};
+#define DIST_VAISSEAU_ASTEROID 300
+typedef enum{PETIT, MOYEN, GRAND}taille_e;
 int TAILLE_ASTEROID[NB_TAILLE]={15,30,50};
 #define VITESSE_ASTEROID 2
 #define FRAME_APPARITION_ASTEROID (10* FRAMES_PER_SECOND)
@@ -36,6 +36,7 @@ int TAILLE_ASTEROID[NB_TAILLE]={15,30,50};
 #define ACCELERATION_SPAWN 0.05
 #define FRAME_2ASTEROID (FRAMES_PER_SECOND/2)
 int PV[NB_TAILLE]={1,2,4};
+SDL_Point coord_spawn[3]={{0,0},{0,(PLAYGROUND_SIZE_H/2)},{(PLAYGROUND_SIZE_W/2),0}};
 
 //missiles
 
@@ -52,6 +53,13 @@ const int FRAME_TIME = 1000 / FRAMES_PER_SECOND;
 
 
 //Bonus
+
+#define NB_BONUS 9
+#define NO_BONUS -1
+#define PROBA_BONUS 2
+#define NB_TIR_MAX 5
+int BONUS_POINT[NB_TAILLE]={5,10,20};
+
 typedef enum{
   TIR_MULTIPLE,
   BOUCLIER,
@@ -59,12 +67,13 @@ typedef enum{
   BONUS_VITESSE_MISSILE,
   DEGAT,
   BOMBE_NUCLEAIRE,
-};
+  POINT_PETIT,
+  POINT_MOYEN,
+  POINT_GRAND
+}bonus_e;
 
-#define NO_BONUS -1
-#define NB_BONUS 6
-#define PROBA_BONUS 2
-#define NB_TIR_MAX 5
+int proba_bonus[NB_BONUS]={3,6,9,12,15,16,22,26,28};
+
 float angle_tir_multiple[NB_TIR_MAX][NB_TIR_MAX]={
   {0,0,0,0,0},
   {-PI/25,PI/25,0,0,0},
@@ -73,7 +82,7 @@ float angle_tir_multiple[NB_TIR_MAX][NB_TIR_MAX]={
   {-PI/4,-PI/8,PI/4,PI/8,0}
 };
 
-#define BONUS_ACCELERATION_MISSILE 1.25
+#define BONUS_ACCELERATION_MISSILE 1.7
 #define VITESSE_MISSILE_MAX (2*VITESSE_MISSILE)
 #define DEGAT_MISSILE_MAX 5
 #define FRAME_BOMBE_NUCLEAIRE (FRAMES_PER_SECOND*1.5)
