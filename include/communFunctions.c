f20cbbb0c418e32198c101c5c0820fa40085f52d
Bniais <hugo.lecomte72@hotmail.fr>
2020-04-14 20:49:01 +0200

    loading adapt size

--- include/communFunctions.c
+++ include/communFunctions.c
@@ -13,9 +13,11 @@ const SDL_Color COLOR_ERROR_LOADING = {0xfb,0x17,0x17};
 const int ALPHA[2*(int)FRAME_ANIM_RETOUR] = { 10, 30, 55, 70, 100, 140, 170, 200, 220, 225, 230, 230, 230, 230, 230, 230,230, 230, 230, 230, 230,230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 230, 225,220,200,170,140,100,70,55,30,10};
 
 void afficherLoading(SDL_Renderer * renderer, SDL_Texture * loadingTexture, SDL_Color color, int shiftX, int shiftY, int frameAnim, int windowW, int windowH){
+
+	float ratioSize = (float)windowW/BASE_WINDOW_W;
 	if(frameAnim){
 		SDL_SetTextureAlphaMod(loadingTexture, 255);
-		SDL_Rect dest = {windowW - 3/2. * LOADING_SIZE + shiftX , windowH - 3/2. * LOADING_SIZE + shiftY, LOADING_SIZE, LOADING_SIZE};
+		SDL_Rect dest = {windowW - 3/2. * ratioSize* LOADING_SIZE + shiftX* ratioSize , windowH - 3/2. * ratioSize* LOADING_SIZE + shiftY* ratioSize, ratioSize* LOADING_SIZE, ratioSize* LOADING_SIZE};
 		SDL_SetTextureColorMod(loadingTexture, color.r, color.g, color.b);
 		SDL_RenderCopyEx(renderer, loadingTexture, &SRC_LOADING, &dest, (frameAnim-1) * LOADING_ANGLE_PER_FRAME, NULL, SDL_FLIP_NONE);
 	}
@@ -23,6 +25,8 @@ void afficherLoading(SDL_Renderer * renderer, SDL_Texture * loadingTexture, SDL_
 
 
 void afficherRetour(SDL_Renderer * renderer, SDL_Texture * loadingTexture,TTF_Font * font, SDL_Color color, int shiftX, int shiftY, int frameAnim, int windowW, int windowH){
+
+	float ratioSize = (float)windowW/BASE_WINDOW_W;
 	int fLogo= frameAnim;
 	if(fLogo < 0){
 		fLogo += FRAME_ANIM_RETOUR;
@@ -32,6 +36,7 @@ void afficherRetour(SDL_Renderer * renderer, SDL_Texture * loadingTexture,TTF_Fo
 
 	//logo
 	if(fLogo){
+
 
 		int w, h;
 		SDL_GetRendererOutputSize(renderer, &w, &h);
@@ -46,7 +51,7 @@ void afficherRetour(SDL_Renderer * renderer, SDL_Texture * loadingTexture,TTF_Fo
 
 		int frameAlpha = -abs(abs(fLogo)-FRAME_ANIM_RETOUR/2) + FRAME_ANIM_RETOUR/2;
 		SDL_SetTextureAlphaMod(loadingTexture, 10+220*frameAlpha/(FRAME_ANIM_RETOUR/2));
-		SDL_Rect dest = {windowW - 3/2. * LOADING_SIZE + shiftX , windowH - 3/2. * LOADING_SIZE + shiftY, LOADING_SIZE, LOADING_SIZE};
+		SDL_Rect dest = {windowW - 3/2. * ratioSize* LOADING_SIZE  + shiftX* ratioSize , windowH - 3/2. * ratioSize* LOADING_SIZE + shiftY* ratioSize, ratioSize* LOADING_SIZE, ratioSize* LOADING_SIZE};
 
 		SDL_RenderCopy(renderer, loadingTexture, &src, &dest);
 
@@ -60,8 +65,8 @@ void afficherRetour(SDL_Renderer * renderer, SDL_Texture * loadingTexture,TTF_Fo
 		SDL_Rect dest;
 		SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
 		SDL_QueryTexture(Message,NULL,(int*)SDL_TEXTUREACCESS_STATIC,&(dest.w), &(dest.h) );
-		dest.w /= (OPEN_FONT_SIZE / SIZE_ECHEC);
-		dest.h /= (OPEN_FONT_SIZE / SIZE_ECHEC);
+		dest.w /= (OPEN_FONT_SIZE / SIZE_ECHEC)* ratioSize;
+		dest.h /= (OPEN_FONT_SIZE / SIZE_ECHEC) *ratioSize;
 
 		//centrer
 		dest.x = PLAYGROUND_SIZE_W/2 - dest.w/2;
