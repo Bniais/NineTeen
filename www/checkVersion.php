2ebaaa07c07b26375af84038b79557f27565933f
Samy <samymahi@me.com>
2020-04-15 02:13:31 +0200

    ajout autorisation travail sur verison beta

--- www/checkVersion.php
+++ www/checkVersion.php
@@ -1,9 +1,10 @@
 <?php
 	$versionActuel = "0.1.4b";
-	
+	$versionBeta = "0.1.5b";
+
 	if( $_SERVER['REQUEST_METHOD'] == 'POST' )
 	{
-		if ( $_POST["version"] == $versionActuel )
+		if ( ($_POST["version"] == $versionActuel) || ($_POST["version"] == $versionBeta) )
 			echo "1";
 		else
 			echo "0";
