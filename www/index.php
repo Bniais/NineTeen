1b9aecb5b5b355b167fd3e3abee4f905411d0bea
Samy <samymahi@me.com>
2020-04-23 16:13:55 +0200

    Web, ajout de la fonction classement avec multiplicateur

--- www/index.php
+++ www/index.php
@@ -1,48 +1,49 @@
-<?php 
+<?php
 
 
 	$recherche = 0;
 	include('include/secure.php');
-	
-	if($_SERVER['REQUEST_METHOD'] == 'GET') 
+
+	if($_SERVER['REQUEST_METHOD'] == 'GET')
 	{
 		$nomRecherche = $_GET["joueur"];
 		if( $nomRecherche != "")
 		{
 			$scoreRecherche[16];
 			$classmentRecherche[16];
-			
+
 			if ( $resultat = $link->query("SELECT SUM(score) AS total FROM nineteen_scores NATURAL JOIN nineteen_players WHERE username = '$nomRecherche' GROUP BY userId ORDER BY total DESC LIMIT 3") )
 				{
 					while ($row = $resultat->fetch_assoc() ) {
 						$scoreRecherche[0] =  $row["total"];
-					} 
-					
-					
+					}
+
+
 					if( $resultat = $link->query("SELECT COUNT(*) FROM (SELECT username, SUM(score) AS total FROM nineteen_scores NATURAL JOIN nineteen_players GROUP BY userId ) AS s WHERE s.total > $scoreRecherche[0]") )
 					{
 
 						while ($row = $resultat->fetch_assoc() ) {
 							$classmentRecherche[0] = $row["COUNT(*)"] + 1;
-						} 
-						
+						}
+
 					}
 				}
-				
-			
+
+
+
 			// RECUPERER 3 MEILLEURE JOUEUR / JEUX
 			for ($i = 1; $i < 16 ; $i++) {
 				if ( $resultat = $link->query("SELECT username,score FROM `nineteen_players` NATURAL JOIN nineteen_scores WHERE gameId = $i AND username = '$nomRecherche' ORDER BY score DESC LIMIT 3") )
 				{
 					while ( $row = $resultat->fetch_assoc()) {
 						$scoreRecherche[$i] = $row["score"];
-					
+
 						if( $resultat = $link->query("SELECT COUNT(*) FROM nineteen_scores WHERE score > $scoreRecherche[$i] AND gameId = $i") )
 							{
 								while ($row = $resultat->fetch_assoc() ) {
 									$classmentRecherche[$i] = $row["COUNT(*)"] + 1;
-							} 
-					
+							}
+
 						}
 					}
 				}
@@ -50,41 +51,42 @@
 			$recherche = 1;
 		}
 	}
-	
+
 	$nom[16][3];
 	$score[16][3];
-	
-	
+
+
 	// RECUPERER 3 MEILLEURS JOUEURS
-	if ( $resultat = $link->query("SELECT username, SUM(score) AS total FROM nineteen_scores NATURAL JOIN nineteen_players GROUP BY userId ORDER BY total DESC LIMIT 3") )
+
+	if ( $resultat = $link->query("SELECT username, FLOOR( SUM( score * multiplicator ) ) AS total FROM nineteen_scores NATURAL JOIN nineteen_multiplicators NATURAL JOIN nineteen_players GROUP BY userId ORDER BY total DESC LIMIT 3") )
 		{
 			$indice = 0;
 			while ($row = $resultat->fetch_assoc() ) {
-				
+
 				$nom[0][$indice] =  $row["username"];
-				
+
 				$score[0][$indice++] =  number_format( $row["total"] , 0, ',', ' ');
-			} 
+			}
 		}
 
 
-	
+
 	// RECUPERER 3 MEILLEURE JOUEUR / JEUX
 	for ($i = 1; $i < 16 ; $i++) {
 		if ( $resultat = $link->query("SELECT username,score FROM `nineteen_players` NATURAL JOIN nineteen_scores WHERE gameId = $i ORDER BY score DESC LIMIT 3") )
 		{
 			$indice = 0;
-			
-			
+
+
 			while ( $row = $resultat->fetch_assoc()) {
 				$nom[$i][$indice] =  $row["username"];
 				$score[$i][$indice++] =  number_format( $row["score"] , 0, ',', ' ');
 			}
 		}
 	}
-	
-	$link->close(); 
-	
+
+	$link->close();
+
 ?>
 
 
@@ -94,14 +96,14 @@
 <!DOCTYPE html>
 <html>
 
-	<?php 
+	<?php
 	$titre = "Nineteen | Accueil";
-	 include('header.php'); 
+	 include('header.php');
 	?>
-	
+
 	<body>
 	<?php include('navigation.php'); ?>
-	
+
 	<h1>MEILLEUR JOUEUR</h1>
 	<h1 class="blink leader" >  <?php echo $nom[0][0]; ?>  </h1>
 	<br>
@@ -112,18 +114,18 @@
 				<input class="recherche" type="text" placeholder="Joueur" name="joueur">
 				<input class="send-rechercher" type="submit" value="Rechercher">
 				<br>
-				<br>	
+				<br>
 				<br>
 			</form>
 	</center>
 
 	<div>
-	
+
 		<div class="container">
-			
+
 			<div class="classement center">
-			
-			
+
+
 				<h1>Classement general</h1>
 				<div class="player1 blink">
 					<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[0][0]; ?></a> <a class="rang-score"><?php echo $score[0][0]; ?></a>
@@ -133,21 +135,21 @@
 				</div>
 				<div class="player2">
 					<a class="rang-classement ">3</a> <a class="rang-name "><?php echo $nom[0][2]; ?></a> <a class="rang-score"><?php echo $score[0][2]; ?></a>
-				</div>	
+				</div>
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[0]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[0]; ?></a>
-				</div>	
-				<?php } ?>				
+				</div>
+				<?php } ?>
 			</div>
-			
+
 		</div>
 
-	
+
 		<div class="container">
 			<div class="jeu left">
-			
-			
+
+
 				<h1>Flappy Easy</h1>
 				<div class="player1 blink">
 					<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[12][0]; ?></a> <a class="rang-score"><?php echo $score[12][0]; ?></a>
@@ -161,8 +163,8 @@
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[12]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[12]; ?></a>
-				</div>	
-				<?php } ?>	
+				</div>
+				<?php } ?>
 				<br>
 				<hr>
 				<h1>Flappy Hard</h1>
@@ -178,15 +180,15 @@
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[1]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[1]; ?></a>
-				</div>	
-				<?php } ?>	
-				
-				
+				</div>
+				<?php } ?>
+
+
 			</div>
-			
+
 			<div class="jeu center">
-			
-			
+
+
 				<h1>Tetris Easy</h1>
 				<div class="player1 blink">
 					<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[11][0]; ?></a> <a class="rang-score"><?php echo $score[11][0]; ?></a>
@@ -200,8 +202,8 @@
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[11]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[11]; ?></a>
-				</div>	
-				<?php } ?>	
+				</div>
+				<?php } ?>
 				<br>
 				<hr>
 				<h1>Tetris Hard</h1>
@@ -217,15 +219,15 @@
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[2]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[2]; ?></a>
-				</div>	
-				<?php } ?>	
-				
+				</div>
+				<?php } ?>
+
 			</div>
-			
-			
+
+
 			<div class="jeu right">
-			
-			
+
+
 				<h1>Asteroid Easy</h1>
 				<div class="player1 blink">
 					<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[10][0]; ?></a> <a class="rang-score"><?php echo $score[10][0]; ?></a>
@@ -239,8 +241,8 @@
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[10]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[10]; ?></a>
-				</div>	
-				<?php } ?>	
+				</div>
+				<?php } ?>
 				<br>
 				<hr>
 				<h1>Asteroid Hard</h1>
@@ -256,18 +258,18 @@
 				<?php if( $recherche ) { ?>
 				<div class="player2">
 					<a class="rang-classement "><?php echo $classmentRecherche[3]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[3]; ?></a>
-				</div>	
-				<?php } ?>	
-				
-				
+				</div>
+				<?php } ?>
+
+
 			</div>
-			
+
 		</div>
-				
+
 	<div class="container">
 		<div class="jeu left">
-		
-		
+
+
 			<h1>Pacman Easy</h1>
 			<div class="player1 blink">
 				<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[9][0]; ?></a> <a class="rang-score"><?php echo $score[9][0]; ?></a>
@@ -281,9 +283,9 @@
 			<?php if( $recherche ) { ?>
 			<div class="player2">
 				<a class="rang-classement "><?php echo $classmentRecherche[9]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[9]; ?></a>
-			</div>	
-			<?php } ?>	
-			
+			</div>
+			<?php } ?>
+
 			<br>
 			<hr>
 			<h1>Pacman Hard</h1>
@@ -299,15 +301,15 @@
 			<?php if( $recherche ) { ?>
 			<div class="player2">
 				<a class="rang-classement "><?php echo $classmentRecherche[4]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[4]; ?></a>
-			</div>	
-			<?php } ?>	
-			
-			
+			</div>
+			<?php } ?>
+
+
 		</div>
-		
+
 		<div class="jeu center">
-		
-		
+
+
 			<h1>Snake Easy</h1>
 			<div class="player1 blink">
 				<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[8][0]; ?></a> <a class="rang-score"><?php echo $score[8][0]; ?></a>
@@ -321,9 +323,9 @@
 			<?php if( $recherche ) { ?>
 			<div class="player2">
 				<a class="rang-classement "><?php echo $classmentRecherche[8]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[8]; ?></a>
-			</div>	
-			<?php } ?>	
-			
+			</div>
+			<?php } ?>
+
 			<br>
 			<hr>
 			<h1>Snake Hard</h1>
@@ -339,16 +341,16 @@
 			<?php if( $recherche ) { ?>
 			<div class="player2">
 				<a class="rang-classement "><?php echo $classmentRecherche[5]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[5]; ?></a>
-			</div>	
-			<?php } ?>	
-			
-			
+			</div>
+			<?php } ?>
+
+
 		</div>
-		
-		
+
+
 		<div class="jeu right">
-		
-		
+
+
 			<h1>Demineur Easy</h1>
 			<div class="player1 blink">
 				<a class="rang-classement ">1</a> <a class="rang-name "><?php echo $nom[7][0]; ?></a> <a class="rang-score"><?php echo $score[7][0]; ?></a>
@@ -362,8 +364,8 @@
 			<?php if( $recherche ) { ?>
 			<div class="player2">
 				<a class="rang-classement "><?php echo $classmentRecherche[7]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[7]; ?></a>
-			</div>	
-			<?php } ?>	
+			</div>
+			<?php } ?>
 			<br>
 			<hr>
 			<h1>Demineur Hard</h1>
@@ -379,11 +381,11 @@
 			<?php if( $recherche ) { ?>
 			<div class="player2">
 				<a class="rang-classement "><?php echo $classmentRecherche[6]; ?></a> <a class="rang-name "><?php echo $_GET["joueur"] ?></a> <a class="rang-score"><?php echo $scoreRecherche[6]; ?></a>
-			</div>	
-			<?php } ?>	
-			
+			</div>
+			<?php } ?>
+
 		</div>
-		
-	</div>		
+
+	</div>
 	</body>
 </html>
