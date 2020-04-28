fcce78f35e2926a5777fbd713c4f7777cc69d0ea
Samy <samymahi@me.com>
2020-04-28 04:26:58 +0200

    code php liee au leaderboard

---
+++ www/leaderboard.php
@@ -0,0 +1,49 @@
+<?php
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST')
+  {
+    include('include/secure.php');
+
+    $gameId = $_POST["gameID"];
+    $offset = $_POST["offset"];
+
+    $username = $_POST["username"];
+
+    if($offset == "")
+    {
+      $offset = 0;
+    }
+    // GLOBAL RECHERCHE
+    if($gameId == "0")
+    {
+      if ( $resultat = $link->query("SELECT username, FLOOR( SUM( score * multiplicator ) ) AS total FROM nineteen_scores NATURAL JOIN nineteen_multiplicators NATURAL JOIN nineteen_players GROUP BY userId ORDER BY total DESC LIMIT 15 OFFSET $offset") )
+        {
+          while ($row = $resultat->fetch_assoc() ) {
+
+            echo  $row["username"];
+            echo "  ";
+            echo  $row["total"];
+            echo " ";
+          }
+        }
+
+
+    }
+    else {
+      if ( $resultat = $link->query("SELECT username,score FROM `nineteen_players` NATURAL JOIN nineteen_scores WHERE gameId = $gameId ORDER BY score DESC LIMIT 15 OFFSET $offset ") )
+  		{
+  			while ( $row = $resultat->fetch_assoc()) {
+          echo  $row["username"];
+          echo " ";
+          echo  $row["score"];
+          echo " ";
+  			}
+  		}
+    }
+
+
+
+    // RECHERCHE LE GAMEID CLASSEMENT
+
+  }
+?>
