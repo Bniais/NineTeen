e34617440adf011dbde46d70fd0deb7968dd58c8
Samy <samymahi@me.com>
2020-05-14 18:15:11 +0200

    ajout de commentaire afin d'expliquer le fonctionnement du hashage

--- exemples/exemple_hash.c
+++ exemples/exemple_hash.c
@@ -4,12 +4,18 @@
 #include <string.h>
 #include <unistd.h>
 
-//#define RAND_MAX 2147483647
+// EXPLICATION DU HASHAGE
+// CE PROGRAMME REPREND LE PRINCIPAL FONCTIONNEMENT
+// DE LA FONCTION DE HASHAGE 
+// 
 
+// IL DEMARRE AVEC UNE VALEUR DEFINI int valeur = ????
+// IL CE CHARGE DE HASHER LA VALEUR EN AFFICHANT LES VALEURS UTILISER 
+// POUR HASHER LA VALEUR
+// IL INCREMENTE ET RE HASH LA VALEUR A CHAQUE FOIS EN VERIFIANT QUE LA VALEUR N AS PAS ETAIT MODFIER
+// SI LA VALEUR EST MODIFIER VIA UN PROGRAMME EXTERNE L EXECUTABLE S ARRETE
 
-//
-//	X = ( ( X * const_1  - const_2 * const_3 ) / const_4 ) 
-//
+// IL EST POSSILE DE REPRODUIRE CECI A L AIDE D'UN LOGICIEL SUR MAC QUI EDITE LES VALEURS EN MEMOIRE "Bit Slicer"
 
 long long hashage (int value, long const_1, long const_2, long const_3, long const_4 ){
 	
@@ -38,7 +44,7 @@ int main() {
 	long long hash;
 	
 	long const_1, const_2, const_3, const_4;
-	init_const(&const_1, &const_2, &const_3, &const_4);
+	init_const_hash(&const_1, &const_2, &const_3, &const_4);
 	printf("CONST_1 = %ld \nCONST_2 = %ld \nCONST_3 = %ld \nCONST_4 = %ld \n",const_1,const_2,const_3,const_4);
 	
 	
@@ -54,7 +60,7 @@ int main() {
 			printf("\nValeur identique : %d",valeur);
 			// si modification necessaire du score
 			// rappel de la fonction init_const pour generer une nouvelle valeur de hashage
-			init_const(&const_1, &const_2, &const_3, &const_4);
+			init_const_hash(&const_1, &const_2, &const_3, &const_4);
 			// nouveau hasage de la valeur changer
 			valeur ++;
 			hash = hashage(valeur, const_1, const_2, const_3, const_4);
