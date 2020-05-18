0ac0162e7ca92308b2a08cf6ce1d0e17ea5479e3
Samy <samymahi@me.com>
2020-05-18 21:24:47 +0200

    check version mise a jour pour version 1.1.2

--- site web/checkVersion.php
+++ site web/checkVersion.php
@@ -1,10 +1,9 @@
 <?php
-	$versionActuel = "1.1.0";
-	$versionBeta = "1.1.1-dev";
+	$versionActuel = "1.1.2";
+	$versionBeta = "1.1.3-dev";
 
 	$versionAutoriser[0] =  $versionBeta;
 	$versionAutoriser[1] =  $versionActuel;
-//	$versionAutoriser[2] =  "1.0.0";
 
 	if( $_SERVER['REQUEST_METHOD'] == 'POST' )
 	{
