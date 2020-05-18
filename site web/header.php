6324957595471c481c3607ede18e82ac13b5a399
Samy <samymahi@me.com>
2020-05-18 21:23:58 +0200

    ameliration de la visibiliter au partage d'un lien

--- site web/header.php
+++ site web/header.php
@@ -1,5 +1,11 @@
 <head>
 	<meta charset="UTF-8">
+	<link rel="icon" href="https://nineteen.recognizer.fr/favicon.ico" />
+	<meta property="og:title" content="Nineteen" />
+	<meta property="og:type" content="website" />
+	<meta property="og:image" content="https://i.ibb.co/ZMGR9qK/preview.png" />
+	<meta property="og:description" content="Redécouvrez les classiques des jeux d'arcades !" />
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<?php if($titre == "Nineteen | Inscription") { ?>
 		<link rel="stylesheet" type="text/css" href="style.css">
